9947153528910788c5874ed79a7a8fd9ca8554f0
fatsermint <kyosti.ahti@gmail.com>
2025-11-10 15:34:43 +0200

    fixed scaling

--- Scenes/texture_rect.gd
+++ Scenes/texture_rect.gd
@@ -7,5 +7,5 @@ var win
 func _process(delta: float) -> void:
 	win = get_window()
 	window_ratio = float(win.size.x) / win.size.y if float(win.size.x) > win.size.y else win.size.y / float(win.size.x)
-	texture_rect.scale.x = window_ratio * 0.5
-	texture_rect.scale.y = window_ratio * 0.5
+	texture_rect.scale.x = window_ratio * 0.9
+	texture_rect.scale.y = window_ratio * 0.9
